c6ed8b51dc4d69fd0efd9e05c9f819037bfcd595
mihir <mihirpamnani31@gmail.com>
2024-01-11 21:33:48 -0600

    updating resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -11,14 +11,14 @@ function Resume() {
       <hr class="dashed" />
 
       <iframe
-        src="https://drive.google.com/file/d/1j-AC5UU5JOZXTNJ1lEXalufZkmGdPR5n/preview"
+        src="https://drive.google.com/file/d/1nxLGP1aFs8xxtQ872iss8KGw_OqZgdyQz6BVOs7VQK4/preview"
         width="640"
         height="825"
         allow="autoplay"
       ></iframe>
 
       <a
-        href="/Resume_Mihir_Pamnani_2023.pdf"
+        href="/portfolio/Resume_Mihir_Pamnani_2023.pdf"
         download="Resume_Mihir_Pamnani_2023.pdf"
         class="clickable"
       >
